df8c12228d697242223b9e218fa6aa19414a9820
arundeveloper26 <arundeveloper.at@gmail.com>
2026-08-08 11:47:49 +0530

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.ts
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        // Basic checks
+        if (s === t) return true;
+    
+        if (s.length !== t.length) return false;
+
+        const sCharCount = {};
+        for (const char of s) {
+            sCharCount[char] = (sCharCount[char] || 0) + 1;
+        }
+
+        const tCharCount = {};
+        for (const char of t) {
+            tCharCount[char] = (tCharCount[char] || 0) + 1;
+        }
+
+        return Object.keys(sCharCount).every(key => sCharCount[key] === tCharCount[key]);
+    }
+}
